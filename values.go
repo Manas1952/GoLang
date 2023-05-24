cea4111d4b57f73f40d1cc5d57d12500903ba0a7
Manas1952 <purohitmanas36@gmail.com>
2023-05-24 21:16:46 +0530

    init

---
+++ values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1 = ", 1+1.111111111111111111111111111)
+	fmt.Println("7.0/3.0 = ", 7/3)
+	fmt.Println(true && true)
+	fmt.Println(!!true)
+	fmt.Println()
+	fmt.Printf("%T", 1+1.11111111111111111111111111)
+}
---
+++ variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "initial"
+	var b, c = 1, "Manas"
+	d := true
+	var e int
+	var f string
+	var g bool
+
+	fmt.Println(a, b, c, d, e, f, g)
+
+}
